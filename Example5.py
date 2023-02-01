5eb2bc364de4896230b88a27dd9601a296d26475
RainBell98 <107784810+RainBell98@users.noreply.github.com>
2023-02-01 09:05:35 +0900

    Add files via upload

---
+++ Example5.py
@@ -0,0 +1,10 @@
+from itertools import combinations
+
+height = [int(input()) for _ in range(9)]
+for co in combinations(height,7):
+    if sum(co) == 100:
+        #print(sorted(co))
+        for height in sorted(co):
+            print(height)
+
+
