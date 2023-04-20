47ad513eda21e7bd699bfedaabd45e26c2220dd4
RainBell98 <107784810+RainBell98@users.noreply.github.com>
2023-04-20 21:21:05 +0900

    Add files via upload

---
+++ SW14413.py
@@ -0,0 +1,14 @@
+T = int(input())
+for i in range(T):
+    a,b = map(int,input().split())
+    p = [list(map(str,input()))for i in range(a)]
+    check = True
+
+
+    for j in range(a):
+        for k in range(b):
+            if
+    if check ==True:
+        print("#{}".format(i+1),'possible')
+    else:
+        print("#{}".format(i + 1), 'impossible')
